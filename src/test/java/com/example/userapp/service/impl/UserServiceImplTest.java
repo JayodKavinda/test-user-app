550b7168eb47c306509ee572af28e13d29b633c3
jayodkavinda <jayodkavinda@gmail.com>
2023-02-05 17:02:50 +0530

    add service layer tests

--- src/test/java/com/example/userapp/service/impl/UserServiceImplTest.java
+++ src/test/java/com/example/userapp/service/impl/UserServiceImplTest.java
@@ -5,11 +5,15 @@ import com.example.userapp.exception.ResourceNotFoundException;
 import com.example.userapp.payload.UserDto;
 import com.example.userapp.repository.UserRepository;
 import org.assertj.core.api.Assertions;
+import org.hibernate.mapping.Any;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +35,65 @@ class UserServiceImplTest {
     private ModelMapper modelMapper;
 
     @Test
+    public void when_SaveUserDto_then_ReturnSameUser() {
+
+        UserDto userDto = UserDto.builder()
+                .firstName("firstNameNew")
+                .lastName("lastNameNew")
+                .email("emailNew@gmail.com")
+                .build();
+
+        User user = User.builder()
+                .firstName("firstNameNew")
+                .lastName("lastNameNew")
+                .email("emailNew@gmail.com")
+                .build();
+        when(userRepository.save(user)).thenReturn(user);
+        when(modelMapper.map(user, UserDto.class)).thenReturn(userDto);
+        when(modelMapper.map(userDto, User.class)).thenReturn(user);
+        UserDto returnUser = userService.saveUser(userDto);
+        Assertions.assertThat(returnUser.getFirstName()).isEqualTo("firstNameNew");
+        Assertions.assertThat(returnUser.getLastName()).isEqualTo("lastNameNew");
+        Assertions.assertThat(returnUser.getEmail()).isEqualTo("emailNew@gmail.com");
+    }
+
+    @Test
+    public void when_updateUserWithId_then_ReturnUpdatedUserObject() {
+        long userId  =1L;
+        UserDto newUserDto = UserDto.builder()
+                .firstName("changedName")
+                .lastName("changedName")
+                .email("emailNew@gmail.com")
+                .build();
+
+        User user = User.builder()
+                .firstName("OrgName")
+                .lastName("OrgName")
+                .email("emailNew@gmail.com")
+                .build();
+
+        User updatedUser = User.builder()
+                .firstName("changedName")
+                .lastName("changedName")
+                .email("emailNew@gmail.com")
+                .build();
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        when(userRepository.save(user)).thenReturn(updatedUser);
+
+
+
+       when(modelMapper.map(updatedUser, UserDto.class)).thenReturn(newUserDto);
+        when(modelMapper.map(newUserDto, User.class)).thenReturn(updatedUser);
+
+        UserDto returnUser = userService.updateUser(newUserDto,userId);
+        verify(userRepository).save(user);
+
+        Assertions.assertThat(returnUser.getFirstName()).isEqualTo("changedName");
+        Assertions.assertThat(returnUser.getLastName()).isEqualTo("changedName");
+        Assertions.assertThat(returnUser.getEmail()).isEqualTo("emailNew@gmail.com");
+    }
+
+    @Test
     public void whenGetByInvalidId_then_ThrowResourceNotFoundException() {
         long userId = 1000L;
         when(userRepository.getById(userId)).thenThrow(new RuntimeException());
@@ -40,7 +103,6 @@ class UserServiceImplTest {
                     userService.getUserById(userId);
                 }).
                 withMessage("user not found for ID: 1000");
-
     }
 
     @Test
@@ -95,9 +157,30 @@ class UserServiceImplTest {
     }
 
     @Test
+    public void when_getUsersWithPagination_then_ReturnUserObject() {
+
+
+        List<User> users = new ArrayList<>();
+        User user1 = User.builder().firstName("Ramesh").lastName("Silva").email("ramesh@gmail.com").build();
+        User user2 = User.builder().firstName("Tony").lastName("Stark").email("tony@gmail.com").build();
+        users.add(user1);
+        users.add(user2);
+
+        Page<User> pages = new PageImpl<User>(users, PageRequest.of(0,2), users.size());
+
+        when(userRepository.findAll(PageRequest.of(0,2))).thenReturn(pages);
+
+
+        Page<User> returnUsers = userService.getUserWithPagination(0,2);
+        Assertions.assertThat(returnUsers.getContent().size()).isEqualTo(users.size());
+        Assertions.assertThat(returnUsers).containsExactlyElementsOf(users);
+
+
+    }
+
+    @Test
     public void when_searchUserByKeyword_then_ReturnsListOfUsers() {
         String q = "keyword";
-
         List<User> users = new ArrayList<>();
         User user1 = User.builder().firstName("keyword").lastName("Silva").email("ramesh@gmail.com").build();
         User user2 = User.builder().firstName("Tony").lastName("keyword").email("tony@gmail.com").build();
@@ -108,6 +191,23 @@ class UserServiceImplTest {
 
         List<User> returnUsers = userService.searchUser(q);
         Assertions.assertThat(returnUsers.size()).isEqualTo(users.size());
+        Assertions.assertThat(returnUsers).containsExactlyElementsOf(users);
+
+    }
+
+    @Test
+    public void when_searchUserByKeywordWithPaging_then_ReturnsListOfUsers() {
+        String q = "keyword";
+        List<User> users = new ArrayList<>();
+        User user1 = User.builder().firstName("keyword").lastName("Silva").email("ramesh@gmail.com").build();
+        User user2 = User.builder().firstName("Tony").lastName("keyword").email("tony@gmail.com").build();
+        users.add(user1);
+        users.add(user2);
+        Page<User> pages = new PageImpl<User>(users, PageRequest.of(0,2), users.size());
+        when(userRepository.findByFirstNameContainingOrLastNameContaining(q,q,PageRequest.of(0,2))).thenReturn(pages);
+
+        Page<User> returnUsers = userService.searchWithPagination(q,PageRequest.of(0,2));
+        Assertions.assertThat(returnUsers.getContent().size()).isEqualTo(users.size());
         Assertions.assertThat(returnUsers).containsExactlyElementsOf(users);
 
     }
